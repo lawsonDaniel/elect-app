9afd673b3d618bc3bb949bb432578100be67e430
daniel lawson <lawblaze4@gmail.com>
2023-11-27 11:06:47 +0100

    auth

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,11 +4,13 @@ import Main from './main'
 import Header from './header'
 import Sidebar from './sidebar'
 import { Box } from '@mui/material'
+import { getAuthUser } from '@/util/auth'
 
 function Page() {
     const [sideBarOpen,setSideBarOpen] = useState<boolean>(false)
     const [activePage,setActivePage] =  useState('overview')
     console.log(activePage,'activepage')
+    console.log(getAuthUser(),'get auth user')
   return (
    <Box className="flex w-full">
         <Sidebar isOpen={sideBarOpen} open={setSideBarOpen} activePage={activePage} setActivePage={setActivePage}/>
