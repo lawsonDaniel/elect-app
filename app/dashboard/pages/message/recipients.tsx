2bb7a663818ded0f4d96b99a43d735c6de98301c
daniel lawson <lawblaze4@gmail.com>
2023-12-02 22:18:00 +0100

    bug fix

--- app/dashboard/pages/message/recipients.tsx
+++ app/dashboard/pages/message/recipients.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Recipients({name,level,profileImg}:any) {
   return (
     <div className="flex flex-col bg-black text-white w-[400px] rounded-lg p-4">
-      <Box className="flex">
+      <Box className="flex gap-4">
       {profileImg && <img className="rounded-[100%] object-cover w-[48px] h-[48px]" width="48" height="48" src={profileImg} alt="user-male-circle"/>}
           <Box>
               <Box>
