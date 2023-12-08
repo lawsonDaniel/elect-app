aaa1b22034371cb4ce9c720a05221173be35147a
daniel lawson <lawblaze4@gmail.com>
2023-12-08 23:41:24 +0100

    added sockect.io to client

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,12 +5,22 @@ import Header from './header';
 import Sidebar from './sidebar';
 import { Box } from '@mui/material';
 import { UserClass } from '../api/user.class';
+import{ Socket } from '../api/socket';
 
 function Page() {
   const [sideBarOpen, setSideBarOpen] = useState<boolean>(false);
   const [activePage, setActivePage] = useState('overview');
   const [user, setUser] = useState<any>(null);
-
+// Socket.emit('send_private_message',{
+//       receiverId:"b7a48b82-d590-4023-9ad5-9722a70b3921",
+//       type:"message",
+//       message:"this is a text from react"
+//     })
+// useEffect(()=>{
+//   Socket.on("receive_private_message",(data)=>{
+//     console.log(data,'recevied message')
+//     })
+// },[Socket])
   useEffect(() => {
     const fetchData = async () => {
       try {
