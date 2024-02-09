1a34439341fe4be293f6d3644bf8bab268f6c235
daniel lawson <lawblaze4@gmail.com>
2024-02-09 08:48:38 +0100

    added students to @copy

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -4,7 +4,16 @@ import React from 'react'
 function Footer() {
   return (
    <Box className="bg-[#000] w-screen h-[200px] flex items-center justify-center text-white">
-        <Typography>copy@2023</Typography>
+        <Typography title="SAMUEL OLUWATIMILEHIN EMMANUEL,
+DANIEL LAWSON OLAOLUWA,
+ALEXANDER MOSES, 
+FRANCIS WISDOM, 
+GAMBO PROMISE ZUGHUMNAAN, 
+AMODU FRANCIS ADEJO, 
+OYALANA JESSE OLUWADAMILOLA, 
+OKWUJIA OYARE VICTOR, 
+DIKKO KABIRU UMARU, 
+AWAJE DANJUMA AJUKO, ">copy@2023</Typography>
    </Box>
   )
 }
